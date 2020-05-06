56e258c2fa78670a90e9a1481111e7d3ebbd0bb8
mkouzmin <mkouzmin@ucsc.edu>
2020-05-05 23:31:47 -0400

    Changes project_CSS to use different heatmap colormap, fixed labels

--- project_CSS.py
+++ project_CSS.py
@@ -55,15 +55,19 @@ print('''<form action="https://bioed.bu.edu/cgi-bin/students_20/mkouzmin/project
     <option value="Class">Class</option>
     <option value="Phylum">Phylum</option>
     </select><br />
-    Mouse Type::<select name="Mouse Type">
+    Mouse Type:<select name="Mouse Type">
+    <option value="All">All</option>
     <option value="Pure">Pure</option>
     <option value="Mix">Mix</option>
-    <option value="All">All</option>
     </select><br />
-    Disease Status::<select name="Disease Status">
+    Disease Status:<select name="Disease Status">
+    <option value="All">All</option>
     <option value="AD">Alzheimer's Disease</option>
     <option value="Wild Type">Wild Type</option>
-    <option value="All">All</option>
+    </select><br />
+    Label Mice as: <select name="Mouse Labels">
+    <option value="ID">MouseIDs</option>
+    <option value="Type">Mouse Types</option>
     </select><br />
 	<input type="submit" value="View HeatMap" name = "heatmap">
 	<input type = "submit" value = "View Stack Bar Plot" name = "stackbar">
@@ -75,22 +79,25 @@ if form:
     Rank = form.getvalue("Rank")
     Type = form.getvalue("Mouse Type")
     Status = form.getvalue("Disease Status")
+    Labels = form.getvalue("Mouse Labels")
     #print(Rank)
     #print(Type)
     #print(Status)
     query = ""
 
+
+
     if Type =="All" and Status == "All":
-        query = """SELECT Abundance.value as value, mouse.name as mname, TaxonomicRank.name as tname FROM Abundance join mouse on Abundance.mid = mouse.mid join TaxonomicRank on Abundance.tid = TaxonomicRank.tid WHERE rank ='%s';""" % (
+        query = """SELECT Abundance.value as value, mouse.name as mname, WTvsAD as type1, PurevsMix as type2, TaxonomicRank.name as tname FROM Abundance join mouse on Abundance.mid = mouse.mid join TaxonomicRank on Abundance.tid = TaxonomicRank.tid WHERE rank ='%s';""" % (
             Rank)
     elif Type == "All":
-        query = """SELECT Abundance.value as value, mouse.name as mname, TaxonomicRank.name as tname FROM Abundance join mouse on Abundance.mid = mouse.mid join TaxonomicRank on Abundance.tid = TaxonomicRank.tid WHERE rank ='%s' and WTvsAD = '%s';""" % (
+        query = """SELECT Abundance.value as value, mouse.name as mname, WTvsAD as type1, PurevsMix as type2,  TaxonomicRank.name as tname FROM Abundance join mouse on Abundance.mid = mouse.mid join TaxonomicRank on Abundance.tid = TaxonomicRank.tid WHERE rank ='%s' and WTvsAD = '%s';""" % (
         Rank, Status)
     elif Status == "All":
-        query = """SELECT Abundance.value as value, mouse.name as mname, TaxonomicRank.name as tname FROM Abundance join mouse on Abundance.mid = mouse.mid join TaxonomicRank on Abundance.tid = TaxonomicRank.tid WHERE rank ='%s' and PurevsMix = '%s';""" % (
+        query = """SELECT Abundance.value as value, mouse.name as mname,  WTvsAD as type1, PurevsMix as type2, TaxonomicRank.name as tname FROM Abundance join mouse on Abundance.mid = mouse.mid join TaxonomicRank on Abundance.tid = TaxonomicRank.tid WHERE rank ='%s' and PurevsMix = '%s';""" % (
             Rank, Type)
     else:
-        query = """SELECT Abundance.value as value, mouse.name as mname, TaxonomicRank.name as tname FROM Abundance join mouse on Abundance.mid = mouse.mid join TaxonomicRank on Abundance.tid = TaxonomicRank.tid WHERE rank ='%s' and PurevsMix = '%s' and WTvsAD = '%s';"""% (Rank,Type,Status)
+        query = """SELECT Abundance.value as value, mouse.name as mname,  WTvsAD as type1, PurevsMix as type2, TaxonomicRank.name as tname FROM Abundance join mouse on Abundance.mid = mouse.mid join TaxonomicRank on Abundance.tid = TaxonomicRank.tid WHERE rank ='%s' and PurevsMix = '%s' and WTvsAD = '%s';"""% (Rank,Type,Status)
 
 
 
@@ -106,31 +113,41 @@ if form:
 
         #print(table)
         if form.getvalue('heatmap'):
-            table = df.pivot(index='tname', columns='mname', values='value')
-            fig, ax = plt.subplots()
+            if Labels == "Type":
+                df['mname'] = df.apply(lambda row: row.mname+row.type1 + row.type2, axis=1)
+                table = df.pivot(index='tname', columns='mname', values='value')
+            else:
+                table = df.pivot(index='tname', columns='mname', values='value')
 
-            ax.pcolor(table.values, cmap=plt.get_cmap('jet'),
-                      vmin=df['value'].min(), vmax=df['value'].max())
+            table = table.apply(lambda x: pow(10, x))
+            fig, ax = plt.subplots()
+            plt.xticks(rotation = 90)
+            ax.pcolor(table.values, vmin = pow(10,df['value'].min()), vmax = pow(10,df['value'].max()),cmap = 'bwr')
             ax.set_xticks(np.arange(table.shape[1] + 1) + 0.5, minor=False)
             ax.set_xticklabels(table.columns, minor=False)
             ax.set_yticks(np.arange(table.shape[0] + 1) + 0.5, minor=False)
             ax.set_yticklabels(table.index, minor=False)
             ax.set_xlim(0, table.shape[1])
             ax.set_ylim(0, table.shape[0])
+            plt.colorbar(ax.pcolor(table.values, vmin = pow(10,df['value'].min()), vmax = pow(10,df['value'].max()), cmap = 'bwr'))
             #plt.show()
             #fig.colorbar(matplotlib.cm.ScalarMappable(cmap=plt.get_cmap('jet')), ax=ax)
             #doesn't work
             figdata = BytesIO()
 
-            fig.savefig(figdata, format='png')
+            fig.savefig(figdata, format='png',bbox_inches='tight')
             image_base64 = base64.b64encode(figdata.getvalue()).decode('utf-8').replace('\n', '')
             figdata.close()
 
             print("<div>")
-            print('''<img src="data:image/png;base64, %s" / >'''%(image_base64))
+            print('''<img src="data:image/png;base64, %s" id = "requested_image"/ >'''%(image_base64))
             print("</div>")
         if form.getvalue('stackbar'):
-            pivot_df = df.pivot(index='mname', columns='tname', values='value')
+            if Labels == "Type":
+                df['mname'] = df.apply(lambda row: row.mname+row.type1 + row.type2, axis=1)
+                pivot_df = df.pivot(index='mname', columns='tname', values='value')
+            else:
+                pivot_df = df.pivot(index='mname', columns='tname', values='value')
             pivot_df = pivot_df.apply(lambda x: pow(10, x))
 
             plot = pivot_df.plot.bar(stacked=True)
@@ -146,11 +163,11 @@ if form:
             plt.legend(loc=6, bbox_to_anchor=(1, 0.5), fontsize='xx-small')
             fig = plot.get_figure()
             figdata = BytesIO()
-            fig.savefig(figdata, format='png')
+            fig.savefig(figdata, format='png',bbox_inches='tight')
             image_base64 = base64.b64encode(figdata.getvalue()).decode('utf-8').replace('\n', '')
             figdata.close()
             print("<div>")
-            print('''<img src="data:image/png;base64, %s" / >''' %(image_base64))
+            print('''<img src="data:image/png;base64, %s", id = requested_image / >''' %(image_base64))
             print("</div>")
     except Exception as mysqlError:
         print("<p><font color=red><b>Error</b> while executing query</font></p>")
